c6ea30523a1a4dec907933a891b791a1528da350
Robbert Noordzij <robbert@xseeding.nl>
2015-03-08 19:59:58 +0100

    Added aliasing on the font rendering

--- src/nl/robbertnoordzij/JFontAwesome/FAIcon.java
+++ src/nl/robbertnoordzij/JFontAwesome/FAIcon.java
@@ -6,6 +6,9 @@ import java.awt.Component;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.Icon;
 
 public class FAIcon implements Icon {
@@ -37,17 +40,22 @@ public class FAIcon implements Icon {
 
 	@Override
 	public void paintIcon(Component c, Graphics g, int x, int y) {
+		Graphics2D graphics = (Graphics2D) g;
+		
+		// Make it appear smoother
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
 		Font resetFont = g.getFont();
 		
 		if (color != null) {
 			// Color does not needed to be reset
-			g.setColor(color);
+			graphics.setColor(color);
 		}
 		
-		g.setFont(font);
-		g.drawString(icon.toString(), x, y + getFontMetrics().getHeight());
+		graphics.setFont(font);
+		graphics.drawString(icon.toString(), x, y + getFontMetrics().getHeight());
 		
-		g.setFont(resetFont);
+		graphics.setFont(resetFont);
 	}
 
 	@Override
